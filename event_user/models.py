c715c9165f5a5cba3ac1e308551455f825379a34
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-06-18 15:03:39 -0700

    completed unique emailand login

--- event_user/models.py
+++ event_user/models.py
@@ -3,7 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-	pass
+  first_name = models.CharField(max_length = 64)
+  last_name = models.CharField(max_length = 64)
+  email=models.EmailField(('email address'), blank=True, unique=True)
+  
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['username']
 
-	def __str__(self):
-		return self.username
+  def __str__(self):
+    return self.username
--- event_user/tests.py
+++ event_user/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import CustomUser
+from django.db.utils import IntegrityError
 
 # Create your tests here.
+class UserTests(TestCase):
+  def setUp(self):
+    self.user = get_user_model().objects.create_user(
+      username='tester',
+      first_name='Tester',
+      last_name='TESTER',
+      email = 'tester@email.com',
+      password = 'password'
+    )
+    self.assertIsInstance(event_user, CustomUser)
+    self.assertEqual(event_user.email, 'tester@email.com')
+    self.assertEqual(event_user.username, 'tester')
+    self.assertIsNotNone(event_user.password)
+
+  # def test_no_dupe_email(self):
+
+  #   user_1 = get_user_model().objects.create_user(
+  #     username='tester',
+  #     first_name='Tester',
+  #     last_name='TESTER',
+  #     email = 'tester@email.com',
+  #     password = 'password'
+  #   )
+  #   try:
+  #     user2 = get_user_model().objects.create_user(
+  #     username='nottester',
+  #     first_name='Nottester',
+  #     last_name='NOTTESTER',
+  #     email = 'tester@email.com',
+  #     password = 'notpassword'
+  #     )
+  #   except IntegrityError:
+  #     print('all good')
+  #   else:
+  #     self.fail('not possible')
