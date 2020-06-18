ec1f86035d1e441ee2886accee40f39273b3c6dc
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-06-18 15:24:46 -0700

    testing completed

--- event_user/tests.py
+++ event_user/tests.py
@@ -14,29 +14,23 @@ class UserTests(TestCase):
       email = 'tester@email.com',
       password = 'password'
     )
-    self.assertIsInstance(event_user, CustomUser)
-    self.assertEqual(event_user.email, 'tester@email.com')
-    self.assertEqual(event_user.username, 'tester')
-    self.assertIsNotNone(event_user.password)
+  def test_method(self):
+    self.assertIsInstance(self.user, CustomUser)
+    self.assertEqual(self.user.email, 'tester@email.com')
+    self.assertEqual(self.user.username, 'tester')
+    self.assertIsNotNone(self.user.password)
 
-  # def test_no_dupe_email(self):
+  def test_no_dupe_email(self):
 
-  #   user_1 = get_user_model().objects.create_user(
-  #     username='tester',
-  #     first_name='Tester',
-  #     last_name='TESTER',
-  #     email = 'tester@email.com',
-  #     password = 'password'
-  #   )
-  #   try:
-  #     user2 = get_user_model().objects.create_user(
-  #     username='nottester',
-  #     first_name='Nottester',
-  #     last_name='NOTTESTER',
-  #     email = 'tester@email.com',
-  #     password = 'notpassword'
-  #     )
-  #   except IntegrityError:
-  #     print('all good')
-  #   else:
-  #     self.fail('not possible')
+    try:
+      user2 = get_user_model().objects.create_user(
+      username='nottester',
+      first_name='Nottester',
+      last_name='NOTTESTER',
+      email = 'tester@email.com',
+      password = 'notpassword'
+      )
+    except IntegrityError:
+      print('all good')
+    else:
+      self.fail('not possible')
